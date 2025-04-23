349621e38d08ddac105e22287fdb41e56c868775
Ahmad Raza <382ahmadraza@gmail.com>
2025-04-23 18:24:55 +0500

    luxery

--- src/app/collections/sale/page.js
+++ src/app/collections/sale/page.js
@@ -1,15 +1,11 @@
-import Navbar from '@/app/common/navbar/Navbar'
 import Title from '@/app/components/title/Title'
-import Footer from '@/app/common/footer/Footer'
 import SaleComp from '@/app/components/saleComp/SaleComp'
 import SaleDeatils from '@/app/components/saleDeatils/SaleDeatils'
 
 export default function page( ) {
   return (
     <div>
-        {/* <Navbar/> */}
         <Title title="SALE" name="Sale" instock="42" outstock="49" component={<SaleComp/>} details={<SaleDeatils/>}/>
-        {/* <Footer/> */}
     </div>
   )
 }
